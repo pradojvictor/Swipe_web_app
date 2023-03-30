a9a19a4db7ff1beb862c028a8fda90545c1c2205
pradojvictor <joaovictorred@hotmail.com>
2023-03-30 02:05:52 -0300

    add import video from local storage

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,6 +1,7 @@
 /* eslint-disable eqeqeq */
 import React, { useRef, useState } from "react";
 import "./video.css"
+import clip from "../videos/clipnatureza.mp4"
 
 function Video() {
 
@@ -26,7 +27,7 @@ function Video() {
                     ref={videoRef}
                     onClick={handdleStart}
                     loop
-                    src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+                    src={clip}
                     >
                 
                 </video>
